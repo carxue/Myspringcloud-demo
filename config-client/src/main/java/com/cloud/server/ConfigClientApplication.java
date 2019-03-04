360343367ece0fc1db57ae44a6e6f9795365ac8a
carxue <xuekui_it@163.com>
2019-03-04 17:52:33 +0800

    config 快速失败和重试代码配置提交

--- config-client/src/main/java/com/cloud/server/ConfigClientApplication.java
+++ config-client/src/main/java/com/cloud/server/ConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.cloud.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.cloud.*" })
 public class ConfigClientApplication {
--- config-server/src/main/java/com/cloud/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/cloud/server/ConfigServerApplication.java
@@ -3,8 +3,10 @@ package com.cloud.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.cloud.*" })
