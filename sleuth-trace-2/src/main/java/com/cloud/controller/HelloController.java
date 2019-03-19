f9f44ac25b693de66c61b13c406e0f8c9e505e30
carxue <xuekui_it@163.com>
2019-03-19 22:13:26 +0800

    logstack日志记录文档提交

--- sleuth-trace-2/src/main/java/com/cloud/controller/HelloController.java
+++ sleuth-trace-2/src/main/java/com/cloud/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.cloud.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,8 +15,8 @@ public class HelloController {
 	
 	@RequestMapping(value="/trace-2",method=RequestMethod.GET)
 	@ResponseBody
-	public String trace() throws InterruptedException{
-		logger.info("==============<call trace-2>=============");
-		return "trace2";
+	public String trace(HttpServletRequest request) throws InterruptedException{
+		logger.info("==============<call trace-2,traceid:"+request.getHeader("X-B3-TraceId")+",spanid:"+request.getHeader("X-B3-SpanId")+">=============");
+		return "trace2"; 
 	}
 }
