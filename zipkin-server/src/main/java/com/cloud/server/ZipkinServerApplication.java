1fe778a1a2591e1a0e50ed0f5eac9e4452533c38
carxue <xuekui_it@163.com>
2019-03-24 22:19:35 +0800

    zipkin收集机制代码提交

--- zipkin-server/src/main/java/com/cloud/server/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/cloud/server/ZipkinServerApplication.java
@@ -2,10 +2,12 @@ package com.cloud.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import org.springframework.context.annotation.ComponentScan;
 
 import zipkin.server.EnableZipkinServer;
 
+@EnableZipkinStreamServer
 @EnableZipkinServer
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.cloud.*" })
